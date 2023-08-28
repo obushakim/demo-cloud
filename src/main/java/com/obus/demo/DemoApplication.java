08cf063d00a17bbaa71d6a8eac92ac9e3c55bfe4
Raissa Amini <raissaamini@gmail.com>
2023-08-29 00:10:11 +0700

    init

---
+++ src/main/java/com/obus/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.obus.demo;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<Object> index(){
+		return ResponseEntity.ok("Welcome");
+	}
+}
---
+++ src/test/java/com/obus/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
